b6bb39b599f312c47ac292b800455c9438f7f122
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-09-21 22:11:50 -0700

    Layout places DropTargets between each component

--- example/basic.jsx
+++ example/basic.jsx
@@ -5,12 +5,24 @@ import HTML5Backend from 'react-dnd-html5-backend';
 import Draggable from './draggabe';
 import Layout from '../src';
 
+const components = [
+  {props: {background: 'red'}},
+  {props: {background: 'green'}},
+  {props: {background: 'blue'}},
+  {props: {background: 'pink'}},
+  {props: {background: 'yellow'}},
+];
+
 class BasicExample extends Component {
   render() {
     return (
-      <div style={{display: 'flex'}}>
-        <Draggable />
-        <Layout />
+      <div>
+        <div style={{display: 'flex'}}>
+          <Draggable />
+          <Draggable />
+          <Draggable />
+        </div>
+        <Layout components={components}/>
       </div>
     );
   }
--- src/layout.jsx
+++ src/layout.jsx
@@ -3,26 +3,40 @@ import React, { Component, PropTypes } from 'react';
 import Target from './target';
 
 class Layout extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
 
-    const components = this.props.components.map((c, idx) => {
+    const components = intersperse(this.props.components.map((c, idx) => {
       return (
-        <div style={{background: c.props.background}}></div>
+        <div style={{background: c.props.background, flex: 1}}></div>
       );
-    });
+    }), <Target />);
 
     return (
-      <div style={{width: 300, height: 300, background: '#ccc', display: 'flex', marginLeft: 100, position: 'relative'}}>
+      <div style={{flex: 1, height: 500, background: '#ccc', display: 'flex', margin: '0 100px', position: 'relative'}}>
         <Target />
-        <div style={{width: 300, background: '#b535e5'}}></div>
-        <Target />
-        <div style={{width: 300, background: '#35b5e5'}}></div>
-        <Target />
-        <div style={{width: 300, background: '#e5b535'}}></div>
+          {components}
         <Target />
       </div>
     );
   }
 }
+
+const intersperse = (arr, el) => {
+    var res = [], i=0;
+    if (i < arr.length)
+        res.push(arr[i++]);
+    while (i < arr.length)
+        res.push(el, arr[i++]);
+    return res;
+};
+
+Layout.propTypes = {
+  components: PropTypes.array
+};
 
 export default Layout;
