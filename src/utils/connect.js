25bd51b8ea1638f7af9ccd4464d4533307627e74
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-10-14 00:48:22 -0700

    updated shouldComponentUpdate in connect

--- src/utils/connect.js
+++ src/utils/connect.js
@@ -1,14 +1,24 @@
 import React, { PropTypes, Component } from 'react';
+import { is, fromJS } from 'immutable';
 
 const connect = mapStateToProps => WrappedComponent => {
-  const LayoutStateContainer = (props, { layoutState, onChange }) => {
-    return (
-      <WrappedComponent
-        {...props}
-        {...mapStateToProps({ state: layoutState, dispatch: onChange }, props)}
-      />
-    );
-  };
+  class LayoutStateContainer extends Component {
+    shouldComponentUpdate(props, state, { layoutState, onChange}) {
+      const mapProps = mapStateToProps({ state: this.context.layoutState, dispatch: this.context.onChange}, this.props);
+      const nextMapProps = mapStateToProps({ state: layoutState, dispatch: onChange }, props);
+      if (!is(fromJS(mapProps), fromJS(nextMapProps))) return true;
+      return false;
+    }
+    render() {
+      const { layoutState, onChange } = this.context;
+      return (
+        <WrappedComponent
+          {...(this.props)}
+          {...mapStateToProps({ state: layoutState, dispatch: onChange }, this.props)}
+        />
+      );
+    }
+  }
 
   LayoutStateContainer.contextTypes = {
     layoutState: PropTypes.object,
