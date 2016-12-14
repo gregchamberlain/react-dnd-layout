54a2a4a0b0a13b8233f69d42e206114a36ee00d5
Greg Chamberlain <gregchamberlain94@gmail.com>
2016-12-13 18:11:01 -0800

    Setup new build and dev environments

--- example/main.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { render } from 'react-dom';
-import ProviderExample from './provider';
-
-render(
-  <ProviderExample />,
-document.getElementById('root'));
---
+++ site/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const rootEl = document.getElementById('root');
+
+render(<div>Hello World!</div>, rootEl);
