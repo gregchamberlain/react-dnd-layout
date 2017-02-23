02f866b6692cd20300d0ed41c9bb6f55080c0a8c
Greg Chamberlain <gregchamberlain94@gmail.com>
2017-02-23 00:01:14 -0800

    Updated testing dependencies, LICENSE

--- tests/model/LayoutState.test.js
+++ tests/model/LayoutState.test.js
@@ -1,3 +1,5 @@
+import expect from 'expect.js';
+
 import LayoutState from '../../src/model/LayoutState';
 
 let state = new LayoutState();
@@ -6,9 +8,13 @@ state.onChange(s => { nextState = s });
 
 let item = { type: 'Test', props: {}, children: [], style: {} };
 
-describe('#addItem', () => {
-  state.addItem('root', 0, item);
-  it('does not mutate original state', () => {
-    expect(nextState).not.toBe(state);
+describe('LayoutState', () => {
+
+  describe('addItem', () => {
+    state.addItem('root', 0, item);
+    it('does not mutate original state', () => {
+      expect(nextState).not.to.equal(state);
+    });
   });
+
 });
